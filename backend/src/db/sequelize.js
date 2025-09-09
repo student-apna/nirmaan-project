1f70408b257b170f4fa9fbdd918893b0c11de9a2
Aftab Alam <aaftabalam18191@gmail.com>
2025-09-09 18:02:05 +0530

    change the DB name

--- backend/src/db/sequelize.js
+++ backend/src/db/sequelize.js
@@ -3,7 +3,7 @@ import createUserModel from "../models/userModel.js";
 
 // Async function to get sequelize instance and models
 export const initDb = async () => {
-  const sequelize = await dbConnection("auth", "postgres", "aftab");
+  const sequelize = await dbConnection("nirmaanDb", "postgres", "aftab");
   const User = createUserModel(sequelize);
   await sequelize.sync({ alter: true });
   console.log("Database & models synced");
